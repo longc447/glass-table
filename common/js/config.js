54295e1ae8f6393935ebc41927760d7fb5a60c1e
楚 <35090745+longc447@users.noreply.github.com>
2023-02-14 01:44:30 +0800

    A:商户认证的时候添加选择服务商户字段

--- common/js/config.js
+++ common/js/config.js
@@ -2,38 +2,38 @@
  * 镜片一站式采购平台
  */
 const x = {
-	// baseUrl: 'https://jqh.tongzhounetwork.ltd',
-	baseUrl: 'https://feifeng.tongzhounetwork.ltd',
-	imgDomain: 'https://feifeng.tongzhounetwork.ltd',
-	h5Domain: 'https://feifeng.tongzhounetwork.ltd/h5',
-	mpKey: 'IB7BZ-4BMCD-43J4O-HS2HW-DZSGF-LGBHX',
-	//小程序appid wx0f57e059962fab6a
+  // baseUrl: 'https://jqh.tongzhounetwork.ltd',
+  baseUrl: 'https://feifeng.tongzhounetwork.ltd',
+  imgDomain: 'https://feifeng.tongzhounetwork.ltd',
+  h5Domain: 'https://feifeng.tongzhounetwork.ltd/h5',
+  mpKey: 'IB7BZ-4BMCD-43J4O-HS2HW-DZSGF-LGBHX',
+  //小程序appid wx0f57e059962fab6a
 }
 const globalConfig = {
-	// 价格保护 3是无保护 0是有保护
-	is_wholesaler: 0,
-	// 是否强制登陆 0不强制 1强制
-	
-	
-	isMustLogin: 0, 
-	// 是否开启定位功能 0关闭 1开启
-	isEnableLocation:0
+  // 价格保护 3是无保护 0是有保护
+  is_wholesaler: 0,
+  // 是否强制登陆 0不强制 1强制
+
+
+  isMustLogin: 0,
+  // 是否开启定位功能 0关闭 1开启
+  isEnableLocation: 0
 }
 let param = {
-	//客服地址
-	webSocket: '',
-	//本地端主动给服务器ping的时间, 0 则不开启 , 单位秒 
-	pingInterval: 1500,
-	// 超时时间 超出时间段将重连系统 单位秒
-	// timeout : 15000,
-	// api安全
-	apiSecurity: false,
-	// 公钥
-	publicKey: ``,
+  //客服地址
+  webSocket: '',
+  //本地端主动给服务器ping的时间, 0 则不开启 , 单位秒 
+  pingInterval: 1500,
+  // 超时时间 超出时间段将重连系统 单位秒
+  // timeout : 15000,
+  // api安全
+  apiSecurity: false,
+  // 公钥
+  publicKey: ``,
 }
 Object.assign(param, x)
 export default {
-	...param,
-	...x,
-	...globalConfig
+  ...param,
+  ...x,
+  ...globalConfig
 }
